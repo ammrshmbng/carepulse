36cb4a21b98345e0e77fb292db4b4c629eb7b3ab
ammrshmbng <ammarmare01@gmail.com>
2024-08-26 19:47:22 +0700

    setup step 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,7 +24,13 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+       <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1 className="text-3xl text-white font-bold underline">Hello</h1>
+   <>
+    <h1 className="text-3xl text-white font-bold underline">Hello</h1>
+   <Button>
+      Click Me
+   </Button>
+   </>
   );
 }
