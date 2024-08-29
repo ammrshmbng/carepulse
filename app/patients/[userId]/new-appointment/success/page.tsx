7f7cebe7d3d08c4dfa4df1d04c50cbd0d5cdd4e6
ammrshmbng <ammarmare01@gmail.com>
2024-08-29 15:41:17 +0700

    appointment feature is complete

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -5,13 +5,18 @@ import { Button } from "@/components/ui/button";
 import { Doctors } from "@/constants";
 import { formatDateTime } from "@/lib/utils";
 import { getUser } from "@/lib/actions/patient.actions";
+import { getAppointment } from "@/lib/actions/appointment.actions";
 
 const RequestSuccess = async ({
   searchParams,
   params: { userId },
 }: SearchParamProps) => {
   
-
+  const appointmentId = (searchParams?.appointmentId as string) || "";
+  const appointment = await getAppointment(appointmentId);
+  const doctor = Doctors.find(
+    (doctor) => doctor.name === appointment.primaryPhysician
+  );
 
   return (
     <div className=" flex h-screen max-h-screen px-[5%]">
@@ -38,6 +43,29 @@ const RequestSuccess = async ({
             been successfully submitted!
           </h2>
           <p>We&apos;ll be in touch shortly to confirm.</p>
+        </section>
+
+        <section className="request-details">
+          <p>Requested appointment details: </p>
+          <div className="flex items-center gap-3">
+            <Image
+              src={doctor?.image!}
+              alt="doctor"
+              width={100}
+              height={100}
+              className="size-6"
+            />
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          </div>
+          <div className="flex gap-2">
+            <Image
+              src="/assets/icons/calendar.svg"
+              height={24}
+              width={24}
+              alt="calendar"
+            />
+            <p> {formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
         </section>
 
         
