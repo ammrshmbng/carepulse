7a063397586236495295a63fa443b283a136cb2c
ammrshmbng <ammarmare01@gmail.com>
2024-08-29 14:00:54 +0700

    success appointment

---
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Doctors } from "@/constants";
+import { formatDateTime } from "@/lib/utils";
+import { getUser } from "@/lib/actions/patient.actions";
+
+const RequestSuccess = async ({
+  searchParams,
+  params: { userId },
+}: SearchParamProps) => {
+  
+
+
+  return (
+    <div className=" flex h-screen max-h-screen px-[5%]">
+      <div className="success-img">
+      <Link href="/">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="h-10 w-fit"
+          />
+        </Link>
+
+        <section className="flex flex-col items-center">
+          <Image
+            src="/assets/gifs/success.gif"
+            height={300}
+            width={280}
+            alt="success"
+          />
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-green-500">appointment request</span> has
+            been successfully submitted!
+          </h2>
+          <p>We&apos;ll be in touch shortly to confirm.</p>
+        </section>
+
+        
+
+      </div>
+    </div>
+  );
+};
+
+export default RequestSuccess;
