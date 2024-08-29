9572d24289f955417509a3a79cf4af17c0090b3a
ammrshmbng <ammarmare01@gmail.com>
2024-08-29 19:49:11 +0700

    stats admin card

---
+++ components/DataTable.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
+import { Button } from "@/components/ui/button";
+
+import { decryptKey } from "@/lib/utils";
+
+
+
+export default function DataTable() {
+
+
+  
+
+  return (
+    <div className="data-table">
+      data table
+   </div>
+  );
+}
