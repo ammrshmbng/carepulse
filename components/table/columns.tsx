7e2f57930495c4750d18ac095aa33fbd91d69042
ammrshmbng <ammarmare01@gmail.com>
2024-08-30 09:16:40 +0700

    patient field

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -29,7 +29,14 @@ export const columns: ColumnDef<Payment>[] = [
       return <p className="text-14-medium ">{row.index + 1}</p>;
     },
   },
-  
+  {
+    accessorKey: "patient",
+    header: "Patient",
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return <p className="text-14-medium ">{appointment.patient.name}</p>;
+    },
+  },
   {
     accessorKey: "status",
     header: "Status",
