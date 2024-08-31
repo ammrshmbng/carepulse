9bbbec39defcc8aeb2b32fc2d7a37687c737f81c
ammrshmbng <ammarmare01@gmail.com>
2024-08-31 12:29:43 +0700

    fix error deployment

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 import {DataTable} from "@/components/table/DataTable";
-import { columns, Payment } from "@/components/table/columns";
+import { columns} from "@/components/table/columns";
 
 
 
